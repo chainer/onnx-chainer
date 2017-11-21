35561ffbc922ee0b3b9ec73d03f03dca587bae1d
Shunta Saito <shunta.saito@gmail.com>
2017-11-21 14:49:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup
 
 setup(
     name='onnx-chainer',
-    version='0.2.1',
+    packages=['onnx_chainer'],
+    version='0.2.1a1',
     description='ONNX support for Chainer',
+    author='Shunta Saito',
+    author_email='shunta@preferred.jp',
     url='https://github.com/mitmul/onnx-chainer',
     keywords='ONNX Chainer model converter deep learning',
     install_requires=['chainer>=2.0.0', 'onnx>=0.2.1'],
-    tests_require=['mxnet>=0.11.0', 'onnx>=0.2', 'numpy'],
-    include_package_data=True,
-    license='Apache 2.0'
+    tests_require=['chainer>=2.0.0', 'onnx>=0.2.1', 'numpy'],
+    license='MIT',
 )
