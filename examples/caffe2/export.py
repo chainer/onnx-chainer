56b4adedaff3706fe85cb5bd4d588b3334d4e134
Shunta Saito <shunta.saito@gmail.com>
2017-12-11 18:39:26 +0900

    Update

--- examples/caffe2/export.py
+++ examples/caffe2/export.py
@@ -12,39 +12,37 @@ from onnx_caffe2.helper import save_caffe2_net
 
 import onnx_chainer
 
+# Instantiate a Chainer model (Chain object)
+model = L.VGG16Layers()
+
+# Prepare a dummy input
+x = np.random.randn(1, 3, 224, 224).astype(np.float32)
+
+# Do not forget setting train flag off!
 chainer.config.train = False
 
-model = L.VGG16Layers()
-x = np.random.randn(1, 3, 224, 224).astype(np.float32)
+# Export to ONNX model
 onnx_model = onnx_chainer.export(model, x)
 
-print(x)
+# Get an output of Chainer model
 y = model(x)
-st = time.time()
-y = model(x)
-print('Chainer:', time.time() - st, 'sec')
 if isinstance(y, dict):
     y = y['prob']
 chainer_out = y.array
 
+# Convert ONNX model to Caffe2 model
 init_net, predict_net = Caffe2Backend.onnx_graph_to_caffe2_net(
     onnx_model.graph, device='CPU')
+
+# Save the Caffe2 model to disk
 init_file = "./vgg16_init.pb"
 predict_file = "./vgg16_predict.pb"
 save_caffe2_net(init_net, init_file, output_txt=False)
 save_caffe2_net(predict_net, predict_file, output_txt=True)
 
-print(x)
+# Get an output of Caffe2 model
 caffe2_out = run_model(onnx_model, [x])[0]
 
-# prepared_backend = prepare(onnx_model)
-# st = time.time()
-# # caffe2_out = prepared_backend.run()
-# # p = workspace.Predictor(init_net, predict_net)
-# # st = time.time()
-# # p = workspace.Predictor(init_net, predict_net)
-# # caffe2_out = p.run([x])[0]
-# print('Caffe2:', time.time() - st, 'sec')
-
+# Check those two outputs have same values
 np.testing.assert_almost_equal(
     chainer_out, caffe2_out, decimal=5)
--- examples/nnvm/export.py
+++ examples/nnvm/export.py
@@ -7,9 +7,8 @@ import chainer.links as L
 import numpy as np
 
 import nnvm
+import nnvm.compiler
 import onnx_chainer
-
-# model = L.VGG16Layers()
 
 
 class MLP(chainer.Chain):
@@ -17,15 +16,20 @@ class MLP(chainer.Chain):
     def __init__(self):
         super().__init__()
         with self.init_scope():
-            self.l1 = L.Linear(None, 10)
+            self.l1 = L.Convolution2D(None, 10, 3, 1, 1)
 
     def __call__(self, x):
         return F.relu(self.l1(x))
 
 
 model = MLP()
-x = np.random.rand(1, 10).astype(np.float32)
+x = np.random.rand(1, 3, 28, 28).astype(np.float32)
 
 chainer.config.train = False
+
 model_onnx = onnx_chainer.export(model, x)
-sym, params = nnvm.frontend.from_onnx(model_onnx.graph)
+
+sym, params = nnvm.frontend.from_onnx(model_onnx)
+target = 'llvm'
+shape_dict = {model_onnx.graph.input[-1].name: x.shape}
+nnvm.compiler.build(sym, target, shape_dict, params=params, dtype="float32")
