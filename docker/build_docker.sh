febb244863bc2fcb8aee3bd0db631222bd7d74a0
Shunta Saito <shunta.saito@gmail.com>
2018-07-05 10:47:18 +0900

    Fix Travis tests

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -14,6 +14,9 @@ docker_build () {
     --build-arg TVM_COMMIT_ID=${tvm_commit_id} \
     --build-arg CHAINER_VERSION=${chainer_version} \
     --build-arg CHAINERCV_VERSION=${chainercv_version} .
+
+    docker push mitmul/onnx-chainer:${docker_tag}
+    docker rmi mitmul/onnx-chainer:${docker_tag}
 }
 
 docker_build 2.7 1.2.0 ebdde3c277a9807a67b233cecfaf6d9f96c0c1bc 3.5.0 0.10.0
--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-run_test () {
-    python_version=$1
-    chainer_version=$2
-    tag_name=python${python_version}-chainer${chainer_version}
-    docker run --rm \
-    -v $PWD:/root/onnx-chainer \
-    -v $PWD/.chainer:/root/.chainer \
-    -ti mitmul/onnx-chainer:${tag_name} \
-    bash -c "cd /root/onnx-chainer && python setup.py develop && py.test -x -s -vvvs tests/"
-}
-
-run_test 2.7 3.5.0
-run_test 3.5 3.5.0
-run_test 3.6 3.5.0
-
-run_test 2.7 4.2.0
-run_test 3.5 4.2.0
-run_test 3.6 4.2.0
-
+python_version=$1
+chainer_version=$2
+tag_name=python${python_version}-chainer${chainer_version}
+docker run --rm \
+-v $PWD:/root/onnx-chainer \
+-v $PWD/.chainer:/root/.chainer \
+-ti mitmul/onnx-chainer:${tag_name} \
+bash -c "cd /root/onnx-chainer && python setup.py develop && py.test -x -s -vvvs tests/"
