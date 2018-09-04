d3f856f78766893391ed83ed12eecdb8c8425274
Shunta Saito <shunta.saito@gmail.com>
2018-09-04 14:59:35 +0900

    Fix a run script of an example

--- examples/mxnet/run.sh
+++ examples/mxnet/run.sh
@@ -3,5 +3,5 @@
 docker run --rm \
 -v $PWD:/root/onnx-chainer \
 -v $PWD/.chainer:/root/.chainer \
--ti mitmul/onnx-chainer:python2.7-chainer3.5.0 \
+-ti mitmul/onnx-chainer:python2.7-chainer4.4.0 \
 bash -c "cd /root/onnx-chainer && python setup.py develop && python examples/mxnet/export.py"
