35f8b3986c3e2072702b59266ce553f19a6f688a
Shunta Saito <shunta.saito@gmail.com>
2018-11-12 19:13:20 +0900

    Update run_all_tests.sh

--- docker/run_all_tests.sh
+++ docker/run_all_tests.sh
@@ -2,8 +2,6 @@
 
 bash docker/run_tests.sh 3.5 5.0.0
 bash docker/run_tests.sh 3.6 5.0.0
-bash docker/run_tests.sh 3.7 5.0.0
 
 bash docker/run_tests.sh 3.5 6.0.0a1
 bash docker/run_tests.sh 3.6 6.0.0a1
-bash docker/run_tests.sh 3.7 6.0.0a1
