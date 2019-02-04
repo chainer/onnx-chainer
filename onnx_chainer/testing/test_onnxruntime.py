a9650c583673293ec9ee3e740ca19109186ac437
Daisuke Tanaka <duaipp@gmail.com>
2019-02-04 18:04:15 +0900

    remove unnecessary comments

--- onnx_chainer/testing/test_onnxruntime.py
+++ onnx_chainer/testing/test_onnxruntime.py
@@ -73,7 +73,7 @@ def check_output(model, x, fn, out_key='prob', opset_version=None):
                          if i.name not in initialized_graph_input_names]
     print(sess.get_modelmeta().custom_metadata_map)
     assert input_names == list(sorted(graph_input_names))
-    # === 
+
     rt_out = sess.run(
         None, {name: array for name, array in zip(input_names, x)})
 
