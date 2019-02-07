409ee59c405737434c9781d2c0354630b258e795
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2019-02-07 22:41:36 +0900

    Add --gpu flag to examples/sample_export.py

--- examples/sample_export.py
+++ examples/sample_export.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
+import chainer
 import numpy as np
 
 import chainercv.links as C
 import onnx_chainer
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=int, default=-1)
+args = parser.parse_args()
+
 model = C.VGG16(pretrained_model='imagenet')
+if args.gpu >= 0:
+    chainer.cuda.get_device_from_id(args.gpu).use()
+    model.to_gpu()
 
 # Pseudo input
-x = np.zeros((1, 3, 224, 224), dtype=np.float32)
+x = model.xp.zeros((1, 3, 224, 224), dtype=np.float32)
 
 onnx_chainer.export(model, x, filename='vgg16.onnx')
