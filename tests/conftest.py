898c0775adfadef8cfb2eb30b3fd34c810c9e569
Daisuke Tanaka <duaipp@gmail.com>
2019-03-15 12:13:56 +0900

    add test runtime option on pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,13 @@ import chainer
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        '--value-check-runtime',
+        dest='value-check-runtime', default='onnxruntime',
+        choices=['skip', 'onnxruntime'], help='select test runtime')
+
+
 @pytest.fixture(scope='function')
 def desable_experimental_warning():
     org_config = chainer.disable_experimental_feature_warning
