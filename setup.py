0cd186bffb6fec51cf54fecf221213bcd58f3313
disktnk <duaipp@gmail.com>
2019-09-28 11:29:21 +0900

    vup

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
         'onnx_chainer.functions',
         'onnx_chainer.testing',
     ],
-    version='1.5.1a1',
+    version='1.5.1a2',
     description='Convert a Chainer model into ONNX',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
