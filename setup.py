492e831912ed689b0130c719dda2db065bb2bc6a
disktnk <duaipp@gmail.com>
2019-11-06 15:55:32 +0900

    fix version num to 1.6.0

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
         'onnx_chainer.functions',
         'onnx_chainer.testing',
     ],
-    version='1.5.1a2',
+    version='1.6.0',
     description='Convert a Chainer model into ONNX',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
